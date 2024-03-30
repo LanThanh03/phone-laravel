fae66b35f6ca2a1467e73d7483d377d1b7f0a089
LanThanh03 <votranlanthanh.1311@gmail.com>
2024-03-30 13:05:25 +0700

    first commit

---
+++ resources/views/frontend/cart/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.checkout')
+
+@section('content')
+    <!-- Shoping Cart Section Begin -->
+    <section class="shoping-cart spad">
+        <div class="container" id="cart">
+        </div>
+    </section>
+    <!-- Shoping Cart Section End -->
+
+@endsection
